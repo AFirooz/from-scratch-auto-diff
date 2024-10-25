4b67fa878e5da8bfd4d02f6cbb5181f809eda745
AFirooz (Home PC) <ali.emad649@gmail.com>
2024-10-25 12:14:15 -0400

    add label argument plus comments to the Value class

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,32 +1,58 @@
 
 class Value:
-    """ stores a single scalar value and its gradient """
+    """ Stores a single scalar value and its gradient.
+    This class is used to represent a value in a computational graph and use that to calculate gradients computationally.
+    """
 
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label:str=None):
+        """
+        Notes (_backward):
+            - The `_backward` now will be a function that will define how the downstream gradient have to be distributed to the children (chained with local gradient) to get the upstream gradient.
+            
+            - by default, we have no operation to perform and no gradient to propagate, so return None. Each mathmatical operation (e.g. `__add__()`) will override this to define its own backward function.
+
+            - Also note that any function defined must add to the old gradient (self.grad) to allow for multiple paths to the same node. In other words, when a node is used multiple times in the graph, we need to accumulate the gradients.
+
+        Notes (_prev):
+            - why use a set?
+            Because we will not need to have duplicates since we will deal with that in the graph creation function trace().
+        """
+
         self.data = data
+        self.label = label
         self.grad = 0
         # internal variables used for autograd graph construction
         self._backward = lambda: None
         self._prev = set(_children)
-        self._op = _op # the op that produced this node, for graphviz / debugging / etc
+        self._op = _op # the operation that produced this node, for graphviz / debugging / etc
 
-    def __add__(self, other):
+    def __add__(self, other): # self + other
+        # to be able to add a number to another Value(number)
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
+        # Define how to distribute the gradient for this operation. Remember `_backward` must be a function
         def _backward():
+            # Note that we are just passing the out.grad (downstream grad) to the children
+            # The local gradient is 1 for both children since out = self + other
+            # so d(out)/d(self) = 1 and d(out)/d(other) = 1
             self.grad += out.grad
             other.grad += out.grad
-        out._backward = _backward
+        out._backward = _backward # update the self._backward function for this operation
 
         return out
 
-    def __mul__(self, other):
+    def __mul__(self, other): # self * other
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
+        # see _backward implementation in __add__() for details
         def _backward():
+            # out = self * other
+            # downstream gradient: out.grad
+            # local grad: d(out)/d(self) = other
             self.grad += other.data * out.grad
+            # local grad: d(out)/d(other) = self
             other.grad += self.data * out.grad
         out._backward = _backward
 
@@ -51,9 +77,27 @@ class Value:
 
         return out
 
-    def backward(self):
+    def sigmoid(self):
+        out = Value(1 / (1 + math.exp(-self.data)), _children=(self,), _op='sigmoid')
 
-        # topological order all of the children in the graph
+        # see _backward implementation in __add__() and __mul__() for details
+        def _backward():
+            """
+            out = sigmoid(self)
+            local gradient: d(out)/d(self) = out * (1 - out)
+            downstream gradient: out.grad
+            """
+            self.grad += out.data * (1 - out.data) * out.grad
+        
+        out._backward = _backward
+        return out
+
+    def _topo_sort(self):
+        """
+        A topological sort (or order) is a graph traversal in which each node v is visited only after all its dependencies are visited.
+        This is needed for implementing the backward() method.
+        We will reverse this list when calling backward(). This way, we ensure that all the parent _backward() are called before the childeren.
+        """
         topo = []
         visited = set()
         def build_topo(v):
@@ -63,7 +107,15 @@ class Value:
                     build_topo(child)
                 topo.append(v)
         build_topo(self)
+        return topo
 
+    def backward(self):
+        """ Perform backpropagation to compute gradients.
+        In other words, it will traverse the graph in reverse order (parents first) and call _backwards() to compute the gradients.
+        
+        Note that the root's gradient needs to be initialized to 1, since we are computing the gradient of the output with respect to itself. Otherwise, since it is zero by default, all children will use zero as the downstream gradient.
+        """
+        topo = self._topo_sort()
         # go one variable at a time and apply the chain rule to get its gradient
         self.grad = 1
         for v in reversed(topo):
@@ -90,5 +142,29 @@ class Value:
     def __rtruediv__(self, other): # other / self
         return other * self**-1
 
+    def __str__(self):
+        """ Used when printing the object """
+        data_grad = f"data={self.data}, grad={self.grad}"
+        
+        # If no children, just return the data and grad info
+        if len(self._prev) == 0:
+            return "Value(" + data_grad + ")\n"
+        operation = f"operation='{self._op}'"
+        
+        # Format with fixed-width fields
+        txt = f"Value({data_grad}, {operation}):\n"
+        
+        # Add children indented and aligned
+        for child in self._prev:
+            child_lines = str(child).split('\n')
+            for j, line in enumerate(child_lines):
+                if line:
+                    # Add two spaces of indentation per level plus a vertical pipe
+                    txt += f"  │ {line}\n"
+        return txt
+
     def __repr__(self):
-        return f"Value(data={self.data}, grad={self.grad})"
+        """ Used when displaying the object """
+        txt = f"Value(data={self.data}, grad={self.grad})" if self.label is None \
+            else f"{self.label}(data={self.data}, grad={self.grad})"
+        return txt
